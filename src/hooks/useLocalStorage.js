f6a3fb0bab43c1a83d945242b84a328fb006111b
unknown <locbgiang@gmail.com>
2020-04-09 15:33:25 -0700

    finished

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -5,7 +5,6 @@ export const useLocalStorage = (key, initialValue) => {
         const item = window.localStorage.getItem(key);
         return item ? JSON.parse(item) : initialValue;
     });
-
     const setValue = value => {
         setStoredValue(value);
         window.localStorage.setItem(key, JSON.stringify(value));
